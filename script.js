4a46c3017b671df0e5ddca2155ec7d4428b24ba7
Jonataraff <jonatarafael209@gmail.com>
2023-11-16 22:32:16 -0300

    feat: adding area kids section

--- script.js
+++ script.js
@@ -39,3 +39,44 @@ function sendMe() {
     email.value = '';
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtém o botão de rolagem para o topo
+  var scrollTopButton = document.getElementById("scrollTopBtn");
+
+  // Adiciona um evento de rolagem à janela
+  window.onscroll = function () {
+  
+  };
+
+  // Adiciona um evento de clique ao botão
+  scrollTopButton.addEventListener("click", function () {
+      // Rola suavemente para o topo
+      scrollToTop(500); // 1000 representa a velocidade da animação em milissegundos
+  });
+
+  // Função para rolar suavemente para o topo
+  function scrollToTop(duration) {
+      var start = window.scrollY,
+          startTime = performance.now();
+
+      function animateScroll() {
+          var now = performance.now(),
+              time = now - startTime,
+              percent = Math.min(time / duration, 1);
+
+          window.scrollTo(0, easeInOutCubic(start, 0, percent));
+
+          if (time < duration) {
+              requestAnimationFrame(animateScroll);
+          }
+      }
+
+      // Função de interpolação para suavizar a animação
+      function easeInOutCubic(start, end, percent) {
+          return start + (3 * percent * percent - 2 * percent * percent * percent) * (end - start);
+      }
+
+      requestAnimationFrame(animateScroll);
+  }
+});
